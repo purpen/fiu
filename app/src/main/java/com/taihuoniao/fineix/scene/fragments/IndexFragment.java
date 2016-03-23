a2692d8f7e02f4b1fbca9655fda5a390124b7d3a
1059232202 <1059232202@qq.com>
2016-03-23 12:10:37 +0800

    create indexfragment

---
+++ app/src/main/java/com/taihuoniao/fineix/scene/fragments/IndexFragment.java
@@ -0,0 +1,36 @@
+package com.taihuoniao.fineix.scene.fragments;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.taihuoniao.fineix.R;
+
+public class IndexFragment extends Fragment {
+
+    public IndexFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_index, container, false);
+    }
+
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+}
