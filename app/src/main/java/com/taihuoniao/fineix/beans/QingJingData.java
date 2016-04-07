4f94653ff4f50b797bf3ec22a3c80fc2a9946b3b
1059232202 <1059232202@qq.com>
2016-04-07 20:37:12 +0800

    "情景数据封装"

--- app/src/main/java/com/taihuoniao/fineix/beans/QingJingData.java
+++ app/src/main/java/com/taihuoniao/fineix/beans/QingJingData.java
@@ -5,9 +5,15 @@ import java.util.ArrayList;
 
 /**
  * @author lilin
- * created at 2016/4/7 19:40
+ *         created at 2016/4/7 19:40
  */
 public class QingJingData implements Serializable {
     public int total_rows;
     public ArrayList<QingJingItem> rows;
+    public int total_page;
+    public int current_page;
+    public String pager;
+    public int next_page;
+    public int prev_page;
+    public int current_user_id;
 }
