a951e7c908b90b562692a4b9062f90e852367d42
1059232202 <1059232202@qq.com>
2016-04-28 10:57:27 +0800

    选择城市

--- app/src/main/java/com/taihuoniao/fineix/beans/ProvinceCityData.java
+++ app/src/main/java/com/taihuoniao/fineix/beans/ProvinceCityData.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
  * @author lilin
  * created at 2016/4/27 13:29
  */
-public class ProvinceCityDistrict {
+public class ProvinceCityData {
     public String areaId;
     public String areaName;
     public ArrayList<City> cities;
