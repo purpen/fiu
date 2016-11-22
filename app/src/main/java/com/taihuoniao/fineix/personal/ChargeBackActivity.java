7d913741d9b4e9e2e0c838f910a7cc1d2b3f011a
Stephen <895745843@qq.com>
2016-11-23 01:43:00 +0800

    修改：退款/售后 2

--- app/src/main/java/com/taihuoniao/fineix/personal/ChargeBackActivity.java
+++ app/src/main/java/com/taihuoniao/fineix/personal/ChargeBackActivity.java
@@ -2,15 +2,15 @@ package com.taihuoniao.fineix.personal;
 
 import android.os.Bundle;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.PopupWindow;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.taihuoniao.fineix.R;
 import com.taihuoniao.fineix.base.BaseActivity;
@@ -27,16 +27,12 @@ import butterknife.OnClick;
 
 public class ChargeBackActivity extends BaseActivity {
 
-    @Bind(R.id.custom_head)
-    CustomHeadView custom_head;
-    @Bind(R.id.editText1)
-    EditText editText1;
-    @Bind(R.id.editText2)
-    EditText editText2;
-    @Bind(R.id.editText3)
-    EditText editText3;
-    @Bind(R.id.button_commit)
-    Button buttonCommit;
+    private CustomHeadView customHead;
+    private EditText editText1;
+    private LinearLayout linearLayoutChargebackCause;
+    private TextView editText2;
+    private EditText editText3;
+    private Button buttonCommit;
 
     private static final String TITLE = "退款";
 
@@ -46,43 +42,71 @@ public class ChargeBackActivity extends BaseActivity {
 
     @Override
     protected void initView() {
-        custom_head.setHeadCenterTxtShow(true, TITLE);
+        customHead = (CustomHeadView) findViewById(R.id.custom_head);
+        editText1 = (EditText) findViewById(R.id.editText1);
+        linearLayoutChargebackCause = (LinearLayout) findViewById(R.id.linearLayout_chargeback_cause);
+        editText2 = (TextView) findViewById(R.id.editText2);
+        editText3 = (EditText) findViewById(R.id.editText3);
+        buttonCommit = (Button) findViewById(R.id.button_commit);
+        customHead.setHeadCenterTxtShow(true, TITLE);
         WindowUtils.chenjin(this);
     }
+//
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        // TODO: add setContentView(...) invocation
+//        ButterKnife.bind(this);
+//    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        // TODO: add setContentView(...) invocation
-        ButterKnife.bind(this);
-    }
-
-    @OnClick({R.id.editText2, R.id.button_commit})
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.editText2:
-                final List<String> list = new ArrayList<>();
-                Collections.addAll(list, chargeBackCause);
-                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ChargeBackActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1,list );
+    protected void installListener() {
+        super.installListener();
+        linearLayoutChargebackCause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity, "弹框", Toast.LENGTH_SHORT).show();
+                List<String> list = new ArrayList<>();
+//                Collections.addAll(list, chargeBackCause);
+                for( int i = 0 ; i  < chargeBackCause.length ; i++) {
+                    list.add(chargeBackCause[i]);
+                }
+                ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(ChargeBackActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, list);
                 ListView listView = new ListView(ChargeBackActivity.this);
                 listView.setAdapter(arrayAdapter);
 
                 PopupWindow popupWindow = new PopupWindow(ChargeBackActivity.this);
                 popupWindow.setContentView(listView);
-                popupWindow.showAsDropDown(view);
+                popupWindow.showAsDropDown(v);
 
                 listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
                     @Override
                     public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                        ((EditText)view).setText(list.get(position));
+                        editText2.setText(chargeBackCause[position]);
                     }
                 });
-                break;
-            case R.id.button_commit:
-                break;
-        }
+            }
+        });
+
+        buttonCommit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(activity, "提交申请", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    private static final String[]  chargeBackCause = {"不喜欢／不想要", "未按约定时间发货", "快递／物流未送到", "快递／物流无跟踪记录"};
+//    @OnClick({R.id.editText2, R.id.button_commit, R.id.linearLayout_chargeback_cause})
+//    public void onClick(View view) {
+//        switch (view.getId()) {
+//            case R.id.linearLayout_chargeback_cause:
+//
+//                break;
+//            case R.id.button_commit:
+//                break;
+//        }
+//    }
+
+    private static final String[] chargeBackCause = {"不喜欢／不想要", "未按约定时间发货", "快递／物流未送到", "快递／物流无跟踪记录"};
 
 }
